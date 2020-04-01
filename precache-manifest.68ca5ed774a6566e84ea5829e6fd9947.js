746ffe23778c50a902ac499b29cfe6277f8a5ac0
Abby Low <abbylow@ieplsg.com>
2020-04-01 11:27:36 +0800

    Updates

---
+++ precache-manifest.68ca5ed774a6566e84ea5829e6fd9947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff42a754fa1088678d0a7c8e03b8fc98",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "6ae433b3590d1b8c9158",
+    "url": "/kanban-board/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f8920e644c84ddb345e6",
+    "url": "/kanban-board/static/js/2.de80afc5.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/kanban-board/static/js/2.de80afc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ae433b3590d1b8c9158",
+    "url": "/kanban-board/static/js/main.6bf700f2.chunk.js"
+  },
+  {
+    "revision": "b22ac035c307b5cc6bd3",
+    "url": "/kanban-board/static/js/runtime-main.bd470873.js"
+  }
+]);
